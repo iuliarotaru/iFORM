c88011a2f9c1de1944b05ba695dbf6e87ab810b0
Iulia Rotaru <iuli0361@stud.kea.dk>
2019-10-10 13:44:20 +0200

    final first page

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function moveCarouselTo(slidePhoto) {
   if (!moving) {
     // temporarily disable interactivity
     disableInteraction();
-    var newPrevious = slidePhoto - 1,
+    let newPrevious = slidePhoto - 1,
       newNext = slidePhoto + 1,
       oldPrevious = slidePhoto - 2,
       oldNext = slidePhoto + 2;
@@ -108,107 +108,39 @@ initCarousel();
 
 //Slide reviews area starts
 //------------------------------------------------
-const slideshow = document.querySelector(".slide-wrap");
+let slideIndex = 1;
+showSlides(slideIndex);
 
-if (slideshow != null) {
-  //make sure we don't run this script if the slideshow is not present
-
-  let slides = document.querySelectorAll(".slide-entry"),
-    slideCount = slides.length,
-    currentSlide = 0,
-    slideHeight = null,
-    initialHeight = slides[0].clientHeight;
-
-  slides[0].classList.add("active"); //on load, activate the first slide
-
-  function moveToSlide(n) {
-    // set up our slide navigation functionality
-    slides[currentSlide].className = "slide-entry";
-    currentSlide = (n + slideCount) % slideCount;
-    slides[currentSlide].className = "slide-entry active";
-    slideHeight = slides[currentSlide].clientHeight;
-    slideshow.style.height = slideHeight + "px";
-    window.addEventListener("resize", function() {
-      resizedSlideHeight = slides[currentSlide].clientHeight;
-      slideshow.style.height = resizedSlideHeight + "px";
-    });
-  }
-
-  function nextSlide(e) {
-    moveToSlide(currentSlide + 1);
-    let code = e.keyCode;
-    if (code == 39) {
-      moveToSlide(currentSlide + 1);
-    }
-  }
-  function prevSlide(e) {
-    moveToSlide(currentSlide + -1);
-    let code = e.keyCode;
-    if (code == 37) {
-      moveToSlide(currentSlide + -1);
-    }
-  }
-
-  const slideHandlers = {
-    nextSlide: function(element) {
-      document.querySelector(element).addEventListener("click", nextSlide);
-      document.body.addEventListener("keydown", nextSlide, false);
-    },
-    prevSlide: function(element) {
-      document.querySelector(element).addEventListener("click", prevSlide);
-      document.body.addEventListener("keydown", prevSlide, false);
-    }
-  };
-
-  slideHandlers.nextSlide("#next-slide");
-  slideHandlers.prevSlide("#prev-slide");
-
-  // Dynamic slideshow height
-
-  slideshow.style.height = initialHeight + "px"; //on load, set the height of the slider to the first active slide
-
-  window.addEventListener("resize", function() {
-    // adjust the height of the slidehow as the browser is resized
-    let resizedHeight = slides[0].clientHeight;
-    slideshow.style.height = resizedHeight + "px";
-  });
-
-  // Detect swipe events for touch devices, credit to Kirupa @ https://www.kirupa.com/html5/detecting_touch_swipe_gestures.htm
-  let initialX = null;
-  let initialY = null;
-  function startTouch(e) {
-    initialX = e.touches[0].clientX;
-    initialY = e.touches[0].clientY;
-  }
-  function moveTouch(e) {
-    if (initialX === null) {
-      return;
-    }
-    if (initialY === null) {
-      return;
-    }
-    let currentX = e.touches[0].clientX;
-    let currentY = e.touches[0].clientY;
-    let diffX = initialX - currentX;
-    let diffY = initialY - currentY;
-    if (Math.abs(diffX) > Math.abs(diffY)) {
-      if (diffX > 0) {
-        // swiped left
-        moveToSlide(currentSlide + 1);
-      } else {
-        // swiped right
-        moveToSlide(currentSlide + -1);
-      }
-    }
-    initialX = null;
-    initialY = null;
-    e.preventDefault();
-  }
-
-  slideshow.addEventListener("touchstart", startTouch, false);
-  slideshow.addEventListener("touchmove", moveTouch, false);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
-//modal interactionts
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+document.getElementById("prevReview").addEventListener("click", function() {
+  plusSlides(-1);
+});
+
+document.getElementById("nextReview").addEventListener("click", function() {
+  plusSlides(1);
+});
 
 const selectSizeBtn = document.querySelector(".selectSizeBtn");
 const selectSizeS = document.querySelector(".selectSize");
